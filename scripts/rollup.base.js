95bf010262173b66d28b8082cb4cf7403088ada4
Arthur Clemens <arthurclemens@gmail.com>
2016-12-23 18:19:47 +0100

    Move dependencies to devDependencies

--- scripts/rollup.base.js
+++ scripts/rollup.base.js
@@ -9,7 +9,9 @@ if (!pkg) {
   throw("Could not read package.json");
 }
 
-const external = Object.keys(pkg.dependencies || {});
+const dependencies = Object.keys(pkg.dependencies || {});
+const devDependencies = Object.keys(pkg.devDependencies || {});
+const external = dependencies.concat(devDependencies);
 
 const globals = {};
 external.forEach(ext => {
