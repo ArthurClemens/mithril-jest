c44ad56ec037e72b22329cd99a9a941ae2d3ecf8
Arthur Clemens <arthurclemens@gmail.com>
2016-12-23 23:11:21 +0100

    Change tidy setting to not ignore empty elements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import htmltidy from "tidy-html5";
 import m from "mithril";
 
 const defaultHtmlTidyOptions = {
-  "show-body-only": "true",
+  "show-body-only": true,
+  "drop-empty-elements": false,
   "doctype": "omit",
   "indent": true,
   "quiet": true, // Hides "About this fork of Tidy ..."
